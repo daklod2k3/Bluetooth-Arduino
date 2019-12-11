6f9697530c15a6955744da429e8af74ea70be232
hayaka <trandacloc123@gmail.com>
2019-12-11 21:09:04 +0700

    update

--- BluetoothArduino/app/src/main/java/com/locdactran/bluetootharduino/MainActivity.java
+++ BluetoothArduino/app/src/main/java/com/locdactran/bluetootharduino/MainActivity.java
@@ -3,25 +3,30 @@ package com.locdactran.bluetootharduino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.IOException;
 import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
-    Button btTrai,btPhai, btTien, btLui, btCheck;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+
+    Button btTrai,btPhai, btCheck, btClose;
+    JoystickView jt;
     TextView t1;
     String address = null, name = null;
     BluetoothAdapter myBluetooth = null;
@@ -37,89 +42,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @SuppressLint("ClickableViewAccessibility")
-    private void setw() throws IOException
+    private void setw()
     {
-        btLui = findViewById(R.id.btLui);
-        btTien = findViewById(R.id.btTien);
+        btCheck = findViewById(R.id.btCheck);
         btTrai = findViewById(R.id.btTrai);
         btPhai = findViewById(R.id.btPhai);
-        btCheck = findViewById(R.id.btCheck);
+        btClose = findViewById(R.id.btClose);
+        btClose.setOnTouchListener(this);
         t1 = findViewById(R.id.tl);
-
+        jt = findViewById(R.id.joystick);
         btCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!myBluetooth.isEnabled()) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, 1);
-                }
-                else{
                     try {
                         bluetooth_connect_device();
-                        t1.setText("Đã kết nối");
+                        btCheck.setText("Close");
                     } catch (IOException e) {
                         t1.setText("Kết nối không thành công");
+                        btCheck.setText("Check");
                     }
+
+            }
+        });
+        btTrai.setOnTouchListener(this);
+        btPhai.setOnTouchListener(this);
+        jt.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                if (angle>45 && angle<135) {
+                    gui((byte) 1);
                 }
-
+                if (angle>=135 && angle<=225) {
+                    gui((byte) 3);
+                }
+                if (angle>225 && angle<315) {
+                    gui((byte) 2);
+                }
+                if ((angle>0 && angle<45)||(angle>=315) && (angle<=360)){
+                    gui((byte) 4);
+                }
+                String vong = String.valueOf(angle);
+                t1.setText(vong);
+                if (strength == 0) {
+                    gui((byte) 0);
+                }
             }
-        });
-
-        bluetooth_connect_device();
-
-        btTien.setOnTouchListener(new View.OnTouchListener()
-        {   @Override
-            public boolean onTouch(View v, MotionEvent event){
-                if(event.getAction() == MotionEvent.ACTION_DOWN) {gui("1");}
-                return true;
-            }
-        });
-
-        btLui.setOnTouchListener(new View.OnTouchListener()
-        {   @Override
-        public boolean onTouch(View v, MotionEvent event){
-            if(event.getAction() == MotionEvent.ACTION_DOWN) {gui("2");}
-            return true;
-        }
-        });
-
-        btPhai.setOnTouchListener(new View.OnTouchListener()
-        {   @Override
-        public boolean onTouch(View v, MotionEvent event){
-            if(event.getAction() == MotionEvent.ACTION_DOWN) {gui("4");}
-            return true;
-        }
-        });
-
-        btTrai.setOnTouchListener(new View.OnTouchListener()
-        {   @Override
-        public boolean onTouch(View v, MotionEvent event){
-            if(event.getAction() == MotionEvent.ACTION_DOWN) {gui("3");}
-            return true;
-        }
         });
 
     }
 
     private void bluetooth_connect_device() throws IOException
     {
-        myBluetooth = BluetoothAdapter.getDefaultAdapter();
-        address = myBluetooth.getAddress();
-        BluetoothDevice hc05 = myBluetooth.getRemoteDevice(address);
-        btSocket = hc05.createInsecureRfcommSocketToServiceRecord(myUUID);
-        btSocket.connect();
-        /*try
+        try
         {
             myBluetooth = BluetoothAdapter.getDefaultAdapter();
+            if (!myBluetooth.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
             address = myBluetooth.getAddress();
             pairedDevices = myBluetooth.getBondedDevices();
             if (pairedDevices.size()>0)
             {
                 for(BluetoothDevice bt : pairedDevices)
                 {
-                    address=bt.getAddress().toString();name = bt.getName().toString();
-                    Toast.makeText(getApplicationContext(),"Connected", Toast.LENGTH_SHORT).show();
-
+                    address=bt.getAddress();name = bt.getName();
                 }
             }
 
@@ -130,28 +117,41 @@ public class MainActivity extends AppCompatActivity {
         btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
         btSocket.connect();
         try { t1.setText("BT Name: "+name+"\nBT Address: "+address); }
-        catch(Exception e){} */
+        catch(Exception e){}
     }
 
-
-
-
-    private void gui(String i)
+    private void gui(Byte i)
     {
         try
         {
             if (btSocket!=null)
             {
-
-                btSocket.getOutputStream().write(i.getBytes());
+                btSocket.getOutputStream().write(i);
             }
-
         }
-        catch (Exception e)
-        {
-            Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
+        catch (Exception e){}
+    }
 
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+            gui((byte) 0);
+        } else {
+                switch (view.getId()) {
+                    case R.id.btTrai:
+                        gui((byte) 5);
+                        break;
+                    case R.id.btPhai:
+                        gui((byte) 6);
+                        break;
+                    case R.id.btClose:
+                        System.exit(0);
+                        break;
+                }
         }
 
+        return false;
     }
 }
+
